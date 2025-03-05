d2ceaaad821cc01e6a88bb1c85aa5a647d1ec6ae
PABLO VALENTIN <pablo.valentin@fatec.sp.gov.br>
2025-03-04 22:31:36 -0300

    feat: adding protection to routes through Laravel Sanctum.
    Adding routes authentication through of personal access tokens, generated by Laravel Sanctum.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+
+            "name" => "required|string",
+            "email" => "required|string|unique:users,email",
+            "password" => "required|string|confirmed"
+            // Apesar de não ser declarado manualmente, há também o campo "password_confirmation", que é passado no Postman.
+
+        ]);
+
+        $user = User::create([
+
+            "name" => $fields["name"],
+            "email" => $fields["email"],
+            "password" => bcrypt($fields["password"])
+
+        ]);
+
+        if($user)
+        {
+            return response()->json([
+
+                "message" => "Usuário cadastrado com sucesso. Faça login para gerar um token.",
+                "user" => $user
+
+            ], 201);
+        }
+
+        else
+        {
+            return response()->json(["message" => "Erro ao cadastrar o usuário!"], 404);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+
+            "email" => "required|string",
+            "password" => "required|string"
+
+        ]);
+
+        $user = User::where("email", $fields["email"])->first();
+
+        if(!$user || !Hash::check($fields["password"], $user->password))
+        {
+            return response()->json([
+
+                "message" => "Erro ao efetuar o login!"
+
+            ], 401);
+        }
+
+        else
+        {
+            $token = $user->createToken("login_token");
+
+            return response()->json([
+
+                "message" => "Login efetuado com sucesso.",
+                "token" => $token->plainTextToken,
+                "user" => $user
+
+            ], 200);
+        }
+    }
+
+    public function logout()
+    {
+        return response()->json(["message" => "Será implementado em breve."], 404);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,60 +1,47 @@
 <?php
 
+// Execute o comando "php artisan route:list" no terminal para visualizar as rotas declaradas.
+
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\
 {
 
+    AuthController,
     TestamentoController,
     LivroController,
     VersiculoController
 
 };
 
-// Execute o comando "php artisan route:list" no terminal para visualizar as rotas declaradas.
+Route::post("user/register", [AuthController::class, "register"]);
+Route::post("user/login", [AuthController::class, "login"]);
+Route::get("user/logout", [AuthController::class, "logout"]);
 
-Route::get("/test", function() {
+// Impedindo que as rotas sejam executadas sem o uso de tokens de acesso do Sanctum.
 
-    return "Rota de teste.";
+Route::middleware("auth:sanctum")->group(function() {
+
+    // Definindo todos os API Resources de uma vez.
+
+    Route::apiResources([
+
+        "testamento" => TestamentoController::class, // Rotas - "Testamento".
+        "livro" => LivroController::class, // Rotas - "Livro".
+        "versiculo" => VersiculoController::class // Rotas - "Versiculo".
+
+    ]);
 
 });
-
-// Definindo todos os API Resources de uma vez.
-
-Route::apiResources([
-
-    "testamento" => TestamentoController::class, // Rotas - "Testamento".
-    "livro" => LivroController::class, // Rotas - "Livro".
-    "versiculo" => VersiculoController::class // Rotas - "Versiculo".
-
-]);
 
 // Módulo - "Testamento".
 
 // Route::apiResource("testamento", TestamentoController::class);
 
-// Route::get("testamento", [TestamentoController::class, "index"]);
-// Route::post("testamento", [TestamentoController::class, "store"]);
-// Route::get("testamento/{id}", [TestamentoController::class, "show"]);
-// Route::put("testamento/{id}", [TestamentoController::class, "update"]);
-// Route::delete("testamento/{id}", [TestamentoController::class, "destroy"]);
-
 // Módulo - "Livro".
 
 // Route::apiResource("livro", LivroController::class);
 
-// Route::get("livro", [LivroController::class, "index"]);
-// Route::post("livro", [LivroController::class, "store"]);
-// Route::get("livro/{id}", [LivroController::class, "show"]);
-// Route::put("livro/{id}", [LivroController::class, "update"]);
-// Route::delete("livro/{id}", [LivroController::class, "destroy"]);
-
 // Módulo - "Versiculo".
 
 // Route::apiResource("versiculo", VersiculoController::class);
-
-// Route::get("versiculo", [VersiculoController::class, "index"]);
-// Route::post("versiculo", [VersiculoController::class, "store"]);
-// Route::get("versiculo/{id}", [VersiculoController::class, "show"]);
-// Route::put("versiculo/{id}", [VersiculoController::class, "update"]);
-// Route::delete("versiculo/{id}", [VersiculoController::class, "destroy"]);
